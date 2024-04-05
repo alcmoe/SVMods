4792ab071401b1a3dabf758ac9da5976aaf9ba63
Alcatraz <9844277+alcmoe@users.noreply.github.com>
2024-04-05 13:22:38 +0800

    init

---
+++ FreezeTime/ConfigMenu.cs
@@ -0,0 +1,44 @@
+﻿using StardewModdingAPI;
+
+namespace FreezeTime
+{
+    public partial class FreezeTime
+    {
+        private ModConfig _config = new();
+        public class ModConfig
+        {
+            public string PauseLogic { get; set; } = "All";
+
+            public bool Any()
+            {
+                return PauseLogic == "Any";
+            }
+        }
+
+        private void GameLaunchedEvent(object? sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+        {
+            _config = Helper.ReadConfig<ModConfig>();
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => _config = new ModConfig(),
+                save: () => Helper.WriteConfig(_config)
+            );
+            configMenu.AddTextOption(
+                mod: this.ModManifest,
+                name: () => "PauseLogic",
+                getValue: () => _config.PauseLogic,
+                setValue: value => _config.PauseLogic = value,
+                allowedValues: ["All", "Any"]
+            );
+        }
+    }
+
+    public interface IGenericModConfigMenuApi
+    {
+        void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
+        void AddTextOption(IManifest mod, Func<string> getValue, Action<string> setValue, Func<string> name, Func<string>? tooltip = null, string[]? allowedValues = null, Func<string, string>? formatAllowedValue = null, string? fieldId = null);
+    }
+}
